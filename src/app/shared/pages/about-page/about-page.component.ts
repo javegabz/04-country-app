2c4fe885b2f385b10685752eb0bcbe8b0c3e3951
javegabz <javegabz@gmail.com>
2024-02-19 21:59:00 +0400

    RouterModule

---
+++ src/app/shared/pages/about-page/about-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-page',
+  templateUrl: './about-page.component.html',
+  styles: [
+  ]
+})
+export class AboutPageComponent {
+
+}
